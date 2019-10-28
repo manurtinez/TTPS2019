0d5e109b2fe6673ee8b92da3970a432191f865db
Pose, Juan Francisco <juanfranciscopose@gmail.com>
2019-10-28 16:07:02 -0300

    agrego tabla recordatorio

--- src/main/java/model/Recordatorio.java
+++ src/main/java/model/Recordatorio.java
@@ -1,12 +1,23 @@
 package model;
 
 import java.util.Date;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "recordatorio")
 public class Recordatorio {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "fecha", nullable = false)
 	private Date fecha;
+	
+	@Column(nullable = false)
 	private String descripcion;
 	
-	
+	public Recordatorio() {}
 	public Recordatorio(Date fecha, String descripcion) {
 		super();
 		this.fecha = fecha;
