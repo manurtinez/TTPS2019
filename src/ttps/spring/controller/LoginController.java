edf3901241c426d703457d84a0446dbbfddee607
manurtinez <mantinez@gmail.com>
2019-12-09 17:17:00 -0300

    pequeña correccion

--- src/ttps/spring/controller/LoginController.java
+++ src/ttps/spring/controller/LoginController.java
@@ -2,6 +2,7 @@ package ttps.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import ttps.spring.services.LoginService;
 
 @RestController
 @RequestMapping
+@CrossOrigin
 public class LoginController {
 	@Autowired
 	private LoginService loginservice;
